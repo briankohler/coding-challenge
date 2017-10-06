cb13d69b2f43744231a06a9f6df69cb631f73584
Brian Kohler <bkohler@kohlermail.com>
2017-10-06 02:08:33 -0700

    update

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return("Hello GumGum!")
+    return("Hello GumGum! version 1.0.0")
 
 @app.route('/health')
 def health():
